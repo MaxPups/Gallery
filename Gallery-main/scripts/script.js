23dedbc5372be0e1ca49e0c993db55e9ab4f4bb9
MaxP-G <77100638+MaxPups@users.noreply.github.com>
2021-06-09 09:51:33 +0300

    Add files via upload

---
+++ Gallery-main/scripts/script.js
@@ -0,0 +1,18 @@
+
+
+
+var columns = document.querySelector(".block-gallery");
+
+
+
+$(function(){
+    $('.like-toggle').click(function(){
+      $(this).toggleClass('like-active');
+      $(this).next().toggleClass('hidden');
+    });
+    local()
+  });
+
+  
+
+
